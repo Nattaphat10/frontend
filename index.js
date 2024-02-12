e63807542f1bfd9f71cd149e8e6630ef03188d7e
Nattaphat10 <s6506021622132@email.kmutnb.ac.th>
2024-02-12 11:25:32 +0700

    dsds

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const axios = require('axios');
-const app = express();
 const path = require("path");
+const app = express();
 var bodyParser = require('body-parser');
 
 const base_url = "http://10.104.9.14";
 
-app.set("views", path.join(__dirname, "/public/views"));
+app.set("views",path.join(__dirname,"/public/views"));
 app.set('view engine','ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
